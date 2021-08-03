7d2c8cf9965633f0eeecd1e35204d27e2bad210b
Edmond Ó Floinn <oflynned@tcd.ie>
2021-08-03 17:33:11 +0200

    chore: add di container

---
+++ app/src/main/java/com/syzible/di/NetworkModule.kt
@@ -0,0 +1,20 @@
+package com.syzible.di
+
+import com.apollographql.apollo.ApolloClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+    @Provides
+    @Singleton
+    fun provideApolloClient(): ApolloClient {
+        return ApolloClient.builder()
+            .serverUrl("https://api.staging.drynk.app/graphql")
+            .build()
+    }
+}
