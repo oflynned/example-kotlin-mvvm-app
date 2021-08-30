6d42dc6fbca5c20ae11f77167a985e7b057014c9
Edmond Ó Floinn <oflynned@tcd.ie>
2021-08-30 11:17:04 +0200

    chore: implement text edit interface

--- app/src/main/java/com/syzible/search/search/SearchFragment.kt
+++ app/src/main/java/com/syzible/search/search/SearchFragment.kt
@@ -1,9 +1,12 @@
 package com.syzible.search.search
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -12,13 +15,17 @@ import com.syzible.search.databinding.FragmentSearchResultsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(), TextWatcher, SearchResultCallback {
+
+    companion object Constants {
+        const val defaultSearchTerm = "Hoegaarden"
+    }
 
     private var _binding: FragmentSearchResultsBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: SearchViewModel by viewModels()
-    private val adapter = SearchAdapter()
+    private val adapter = SearchAdapter(this)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,19 +40,41 @@ class SearchFragment : Fragment() {
 
         binding.searchResultRecyclerView.layoutManager = LinearLayoutManager(context)
         binding.searchResultRecyclerView.adapter = adapter
-
-        lifecycleScope.launchWhenCreated {
-            viewModel.search("hoe")
-        }
+        binding.searchBoxEditText.addTextChangedListener(this)
 
         viewModel.searchResults.observe(viewLifecycleOwner, { results ->
             adapter.setList(results)
             adapter.notifyDataSetChanged()
         })
+
+        lifecycleScope.launchWhenCreated {
+            binding.searchBoxEditText.setText(defaultSearchTerm)
+            viewModel.search(defaultSearchTerm)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        if (!s.isNullOrEmpty()) {
+            lifecycleScope.launchWhenCreated {
+                viewModel.search(s.toString())
+            }
+        }
+    }
+
+    override fun onClick(result: SearchResult) {
+        Toast.makeText(context, result.name, Toast.LENGTH_SHORT).show()
     }
 }
