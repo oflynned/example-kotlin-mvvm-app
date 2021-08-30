4dba06e3660b37423ee519a8c45415975966d150
Edmond Ó Floinn <oflynned@tcd.ie>
2021-08-30 16:53:30 +0200

    chore: create view state

---
+++ app/src/main/java/com/syzible/search/utils/ViewState.kt
@@ -0,0 +1,7 @@
+package com.syzible.search.utils
+
+sealed class ViewState<T>(val value: T? = null, val message: String? = null) {
+    class Success<T>(data: T) : ViewState<T>(data)
+    class Error<T>(message: String?, data: T? = null) : ViewState<T>(data, message)
+    class Loading<T> : ViewState<T>()
+}
