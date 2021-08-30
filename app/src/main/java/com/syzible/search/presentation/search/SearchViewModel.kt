92ed2fe6877ab462acd2c107546c3d1d32610b95
Edmond Ó Floinn <oflynned@tcd.ie>
2021-08-30 16:53:40 +0200

    chore: emit view state

--- app/src/main/java/com/syzible/search/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/syzible/search/presentation/search/SearchViewModel.kt
@@ -3,8 +3,10 @@ package com.syzible.search.presentation.search
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.apollographql.apollo.exception.ApolloException
 import com.syzible.search.data.SearchResult
 import com.syzible.search.domain.GetSearchResultsUseCase
+import com.syzible.search.utils.ViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -13,12 +15,15 @@ class SearchViewModel @Inject constructor(
     private val getSearchResultsUseCase: GetSearchResultsUseCase
 ) : ViewModel() {
 
-    private val _searchResults: MutableLiveData<List<SearchResult>> = MutableLiveData()
-    val searchResults: LiveData<List<SearchResult>> get() = _searchResults
+    private val _searchResults: MutableLiveData<ViewState<List<SearchResult>>> = MutableLiveData()
+    val searchResults: LiveData<ViewState<List<SearchResult>>> get() = _searchResults
 
     suspend fun search(term: String) {
-        val results = getSearchResultsUseCase.execute(term)
-
-        _searchResults.postValue(results)
+        try {
+            val results = getSearchResultsUseCase.execute(term)
+            _searchResults.postValue(ViewState.Success(results))
+        } catch (e: ApolloException) {
+            _searchResults.postValue(ViewState.Error(e.message))
+        }
     }
 }
