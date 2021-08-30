fd89916de50c0256f382c505057cd3a69b3455fa
Edmond Ó Floinn <oflynned@tcd.ie>
2021-08-30 12:16:19 +0200

    chore: show args in detail view card

--- app/src/main/java/com/syzible/search/detailview/DetailViewFragment.kt
+++ app/src/main/java/com/syzible/search/detailview/DetailViewFragment.kt
@@ -1,37 +1,47 @@
 package com.syzible.search.detailview
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.syzible.search.R
+import androidx.navigation.fragment.navArgs
 import com.syzible.search.databinding.FragmentSearchDetailViewBinding
+import com.syzible.search.type.DrinkCategory
 
 class DetailViewFragment : Fragment() {
 
     private var _binding: FragmentSearchDetailViewBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val args: DetailViewFragmentArgs by navArgs()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentSearchDetailViewBinding.inflate(inflater, container, false)
-        return binding.root
 
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_detail_view_to_search_results)
+            findNavController().popBackStack()
+        }
+
+        binding.name.text = args.name
+        binding.abv.text = args.abv
+        binding.category.text = DrinkCategory.BEER.name
+
+        if (args.styles.isEmpty()) {
+            binding.styles.visibility = GONE
+        } else {
+            binding.styles.text = args.styles.joinToString(", ")
         }
     }
 
